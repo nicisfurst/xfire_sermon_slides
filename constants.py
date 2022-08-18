cd57be4d2c4f7b7f337b25ed4b1a2c9d03e21d2f
Nic Furst <nicisfurst@gmail.com>
2022-08-19 01:25:55 +1000

    refactor

---
+++ constants.py
@@ -0,0 +1,13 @@
+"""
+Outlines the constants used across the repo
+"""
+
+# Logging
+LOGGING_FORMATTER = '[%(asctime)s %(filename)s->%(funcName)s():%(lineno)s]%(levelname)s: %(message)s'
+# REL_LOGS_DIR = 'logs'
+
+# Directories and Files
+FONT_DIR = 'fonts'
+BG_DIR = 'backgrounds'
+SLIDE_DIR = 'slides'
+SETUP_DATA = 'setup.json'
